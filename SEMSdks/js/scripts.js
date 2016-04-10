7513feeb93bfacee0ce462bf07ced464765b5b8d
ashishmvvt <vmondret@kent.edu>
2016-04-09 21:00:07 -0400

    initial commit of UI

---
+++ SEMSdks/js/scripts.js
@@ -0,0 +1,12 @@
+/***************** Waypoints ******************/
+
+$(document).ready(function() {
+
+	$('.wp1').waypoint(function() {
+		$('.wp1').addClass('animated fadeInLeft');
+	}, {
+		offset: '75%'
+	});
+	
+
+});
