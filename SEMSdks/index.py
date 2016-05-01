964cc181c1c5de0213e6e031bc416ba2f02c71f1
Bhaskar Thadisetty <16.bhaskar@gmail.com>
2016-04-30 23:40:15 -0400

    Updated Index

--- SEMSdks/index.py
+++ SEMSdks/index.py
@@ -1,29 +1,8 @@
-
-from bottle import run, route, view, app, hook
-from bottle import get, put, post, request, redirect, template
-from bottle import static_file, error, debug
-from functools import wraps
-from beaker.middleware import SessionMiddleware
+from bottle import get, put, post, request, redirect, run, route, view, app, hook, template, static_file, error, debug
 import json
-import sys, os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/src/Controller")
-from BoardController import *
-
 from bottle import default_app, route
 
-
 app = app()
-session_opts = {
-    'session.auto': True,
-    'session.cookie_expires': True,
-    'session.encrypt_key': 'please use a random key and keep it secret!',
-    'session.timeout': 3600 * 24,  # 1 day
-    'session.type': 'cookie',
-    'session.validate_key': True,
-    'session.httponly': True,
-}
-application = SessionMiddleware(app, session_opts)
-
 
 @route('/')
 @validate
@@ -38,27 +17,25 @@ def new_game():
 
 @post('/modify')
 @validate
-def update_handler():
+def update():
     return update()
 
 @route('/stop')
 @validate
-def end_game():
+def endgame():
     return end()
 
 @route('/status')
 @validate
-def game_status():
+def status():
     return status()
 
 @route('/<filename:path>')
-def send_static(filename):
+def static(filename):
     return static_file(filename, root='static/')
 
-
 def main():
-    debug(True)
-    run(app=application, quiet=False, reloader=True)
+    run(app=application, reloader=True)
 
 if __name__ == "__main__":
     main()
